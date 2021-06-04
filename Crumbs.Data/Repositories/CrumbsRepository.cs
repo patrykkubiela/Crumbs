1866f01d1efd47d5decc8df200c9d50a00da4961
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-06-04 13:28:47 +0200

    user cert validation always true

--- Crumbs.Data/Repositories/CrumbsRepository.cs
+++ Crumbs.Data/Repositories/CrumbsRepository.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using Crumbs.Data.Models;
 using Dapper;
 
@@ -10,8 +12,14 @@ namespace Crumbs.Data.Repositories
         public ICollection<Crumb> GetCrumbs(string query)
         {
             using var connection = PostgresDbConnectionProvider.GetDbConnection();
+            connection.UserCertificateValidationCallback = UserCertificateValidationCallback;
             var events = connection.Query<Crumb>(query).ToList();
             return events;
+        }
+
+        private bool UserCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslpolicyerrors)
+        {
+            return true;
         }
     }
 }
