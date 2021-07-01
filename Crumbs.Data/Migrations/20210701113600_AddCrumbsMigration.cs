68596dc89744dc3df38f2a6d22daf666db2707ef
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-07-01 13:44:35 +0200

    first ef migration - add crumbs

---
+++ Crumbs.Data/Migrations/20210701113600_AddCrumbsMigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Crumbs.Data.Migrations
+{
+    public partial class AddCrumbsMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Crumbs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    BroadcasterId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Crumbs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Crumbs_Crumbs_BroadcasterId",
+                        column: x => x.BroadcasterId,
+                        principalTable: "Crumbs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Crumbs_BroadcasterId",
+                table: "Crumbs",
+                column: "BroadcasterId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Crumbs");
+        }
+    }
+}
