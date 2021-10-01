088599b7376b6fd92c85f0fcca91dd3501abbaa0
Patryk Kubiela <patryk.kubiela@programista-doswiadczony.pl>
2021-10-01 19:51:19 +0200

    remove controller middleweare auth policy

--- Crumbs.Api/Controllers/CrumbController.cs
+++ Crumbs.Api/Controllers/CrumbController.cs
@@ -1,14 +1,12 @@
 using System.Threading;
 using Crumbs.Api.Interfaces;
 using Crumbs.Data.Models;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Crumbs.Api.Controllers
 {
     [ApiController]
     [Route("crumb")]
-    [Authorize(Policy = "User")]
     public class CrumbController : ControllerBase
     {
         private readonly ICrumbsManager _crumbsManager;
